b191fde38cfbd986d37b3a8dc14f1e6101c0afd2
kariih0107 <xxkariihxx@gmail.com>
2024-07-30 21:06:06 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function showMessage() {
+    const message = document.getElementById('message');
+    message.style.opacity = '1';
+    setTimeout(() => {
+        message.style.opacity = '0';
+    }, 2000);
+}
