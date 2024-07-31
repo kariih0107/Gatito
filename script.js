b47669396b16ac7fba79016b6ff39eefde46aa36
kariih0107 <xxkariihxx@gmail.com>
2024-07-30 21:16:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-function showMessage() {
-    const message = document.getElementById('message');
-    message.style.display = 'block'; // Muestra el mensaje
-    message.style.opacity = 1; // Asegura que el mensaje sea visible
-    message.style.transition = 'opacity 0.5s'; // Transición suave
 
-    // Oculta el mensaje después de 2 segundos
-    setTimeout(() => {
-        message.style.opacity = 0; // Desvanece el mensaje
-        setTimeout(() => {
-            message.style.display = 'none'; // Oculta el mensaje
-        }, 500); // Espera a que termine la animación de desvanecimiento
-    }, 2000); // Mensaje visible durante 2 segundos
-}
